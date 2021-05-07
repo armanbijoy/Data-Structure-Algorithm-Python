ed4037240c4187a3d1eaeb189ca3745ef3661366
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-07 22:48:27 +0600

    Changes Added

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,8 +1,9 @@
 
 def fact(n):
+    assert n>=0 and int(n) == n,'The Number Must be positive Number'
     if n in [0,1]:
         return 1;
     else:
         return n * fact(n-1);
 
-print(fact(3))
+print(fact(6))
