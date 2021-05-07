f78d65eaee5fd9fd06303a3668cca3034ec61a23
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-07 23:42:13 +0600

    Added

---
+++ interview ques about recursion/sum of digits.py
@@ -0,0 +1,7 @@
+def sumofDigits(n):
+     assert n>=0 and int(n) == n,'The Number Must be positive & Integer Number'
+     if n == 0:
+         return 0
+     else:
+        return int(n%10) + sumofDigits(int(n/10))
+print(sumofDigits(54))
