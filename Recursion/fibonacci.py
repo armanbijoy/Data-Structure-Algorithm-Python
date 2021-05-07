5a6a6d5c0c16fed44309d24534c4748d02ee2221
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-07 23:23:48 +0600

    added

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,9 +1,9 @@
-
-def fact(n):
+def fibonacci(n):
     assert n>=0 and int(n) == n,'The Number Must be positive & Integer Number'
     if n in [0,1]:
-        return 1;
+        return n
     else:
-        return n * fact(n-1);
+         return fibonacci(n-1)+fibonacci(n-2)
+   
 
-print(fact(0))
+print(fibonacci(5))
