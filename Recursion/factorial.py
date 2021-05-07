aec1a0c43cebeb2d4ee2430187f2745fd8cd907e
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-07 22:53:31 +0600

    Added
    Signed-off-by: Arman Bijoy <armanbijoy4@gmail.com>

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,9 +1,9 @@
 
 def fact(n):
-    assert n>=0 and int(n) == n,'The Number Must be positive Number'
+    assert n<=0 and int(n) == n,'The Number Must be positive & Integer Number'
     if n in [0,1]:
         return 1;
     else:
         return n * fact(n-1);
 
-print(fact(6))
+print(fact(0))
